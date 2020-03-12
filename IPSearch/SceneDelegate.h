cc4c9a92795cdcd28b5f4929021d5856e770e55a
isoloman <1013481871@qq.com>
2020-03-12 14:44:08 +0800

    Initial Commit

---
+++ IPSearch/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  IPSearch
+//
+//  Created by Gloryyin on 2020/3/12.
+//  Copyright © 2020 Gloryyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ IPSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IPSearch
+//
+//  Created by Gloryyin on 2020/3/12.
+//  Copyright © 2020 Gloryyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
